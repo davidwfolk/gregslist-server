c2c5de478fd4c8d0a12307f280c07d16a810e9d3
David Folk <davidwfolk@gmail.com>
2020-04-07 16:47:33 -0600

    stuck on the put & delete

--- server/controllers/CarsController.js
+++ server/controllers/CarsController.js
@@ -9,10 +9,13 @@ export class CarsController extends BaseController {
   constructor() {
     super("api/cars")
     this.router
-      .get("", this.getAll)
-      .post('', this.create)
+    .get("", this.getAll)
+    .get('/:id', this.getCarById)
+    .post('', this.create)
+    .put('/:id', this.updateCar)
+    .delete('/:id', this.deleteCar)
   }
-
+  
   async getAll(req, res, next) {
     try {
       let cars = await carsService.getAll()
@@ -20,13 +23,20 @@ export class CarsController extends BaseController {
     } catch (error) {
       next(error)
     }
-
+    
     //NOTE these are the same
     // carsService.getAll().then(cars=>{
-    // res.send(cars)
-    // }).catch(err => next(err))
+      // res.send(cars)
+      // }).catch(err => next(err))
+    }
+  async getCarById(req, res, next) {
+    try {
+      let car = await carsService.findById(req.params.id)
+      res.send({ data: car, message: "here's your car!" })
+    } catch (error) {
+      next(error)
+    }
   }
-
   async create(req, res, next) {
     try {
       let car = await carsService.create(req.body)
@@ -36,6 +46,23 @@ export class CarsController extends BaseController {
     }
   }
 
+  async updateCar(req, res, next) {
+    try {
+      let editedCar = await carsService.updateCar(req.params.id, req.body)
+      req.send({data: editedCar, new: true, message: "Car updated!"})
+    } catch (error) {
+      
+    }
+  }
+    
+  async deleteCar (req, res, next){
+    try {
+      let deleteCar = await carsService.deleteCar(req.params.id)
+      res.delete({data: deleteCar, message:"#ClickClick - Deleted"})
+    } catch (error) {
+      
+    }
+  }
 
 
 }
--- server/services/CarsService.js
+++ server/services/CarsService.js
@@ -2,6 +2,14 @@ import { dbContext } from "../db/DbContext";
 
 
 class CarsService {
+  async deleteCar(id) {
+    // let cars = await dbContext.Cars.deleteOne({_id: id})
+    let cars = await dbContext.Cars.findByIdAndDelete(id)
+
+  }
+  async findById(id) {
+    let car = await dbContext.Cars.findById(id)
+  }
   async create(body) {
     let car = await dbContext.Cars.create(body)
     return car
@@ -10,6 +18,12 @@ class CarsService {
   async getAll() {
     let cars = await dbContext.Cars.find()
     return cars
+  }
+
+  async updateCar(id, updatedCar) {
+    let cars = await dbContext.Cars.findByIdAndUpdate(id, updatedCar)
+    return cars
+    // updatedCar = body
   }
 }
 
